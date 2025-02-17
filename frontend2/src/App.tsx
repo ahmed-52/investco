30a55f25a1b7c533455a3d7ac871c355101555fe
Ahmed Abdulla <aaa384@cornell.edu>
2025-02-17 12:38:51 -0500

    issues fixes

--- frontend2/src/App.tsx
+++ frontend2/src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './Dashboard';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
