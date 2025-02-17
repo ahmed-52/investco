7264703db053bd6a0a4151106bb2a0964c96e309
Ahmed Abdulla <aaa384@cornell.edu>
2025-02-17 12:39:17 -0500

    deleted frontend from main

--- frontend2/src/theme/customizations.ts
+++
@@ -1,9 +0,0 @@
-import type { ThemeOptions } from "@mui/material/styles";
-
-export const chartsCustomizations: ThemeOptions["components"] = {};
-
-export const dataGridCustomizations: ThemeOptions["components"] = {};
-
-export const datePickersCustomizations: ThemeOptions["components"] = {};
-
-export const treeViewCustomizations: ThemeOptions["components"] = {};
